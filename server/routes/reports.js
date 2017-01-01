ebe4789fb8b0c4a64c300236a1a076247313a748
Michael Kohler <mkohler@picobudget.com>
2017-01-01 18:20:08 +0100

    Add POST method for /reports (without any auth checks)

--- server/routes/reports.js
+++ server/routes/reports.js
@@ -10,4 +10,16 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.post('/', (req, res, next) => {
+  const Report = mongoose.model('Report');
+
+  let newReport = new Report();
+  newReport.date = req.body.date || new Date();
+  newReport.title = req.body.title;
+  newReport.description = req.body.description;
+  newReport.save();
+
+  res.json(newReport);
+});
+
 module.exports = router;
